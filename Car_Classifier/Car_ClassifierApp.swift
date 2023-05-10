0596165d90213db65f06c75a398f250333362f66
Aphaki <kiapha1108@gmail.com>
2023-05-10 15:41:50 +0900

    Initial Commit

---
+++ Car_Classifier/Car_ClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_ClassifierApp.swift
+//  Car_Classifier
+//
+//  Created by Sy Lee on 2023/05/10.
+//
+
+import SwiftUI
+
+@main
+struct Car_ClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
